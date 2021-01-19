28314a61296745d9132464a6e890a2a9a22e113c
Aleksandra Grahova <grahova_sasha@mail.ru>
2021-01-19 21:21:41 +0300

    Накидала верстку главной страницы

--- arzeda.front/src/App.vue
+++ arzeda.front/src/App.vue
@@ -1,53 +1,204 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Header></Header>
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <h2 class="mb-2">Рестораны</h2>
+        <v-text-field
+            outlined
+            label="Название, кухня или блюдо..."
+            prepend-inner-icon="mdi-magnify"
+          ></v-text-field>
+          <v-chip-group active-class="blue white--text">
+            <v-chip class="ma-2">Все</v-chip>
+            <v-chip class="ma-2">Бургеры</v-chip>
+            <v-chip class="ma-2">Суши</v-chip>
+            <v-chip class="ma-2">Пицца</v-chip>
+            <v-chip class="ma-2">Десерты</v-chip>
+            <v-chip class="ma-2">Шашлыки</v-chip>
+            <v-chip class="ma-2">Здоровая еда</v-chip>
+            <v-chip class="ma-2">Мясо и рыба</v-chip>
+          </v-chip-group>
+          <div class="grid-container">
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+            
+            <v-card
+              class="mx-auto"
+              max-width="344"
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="160px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-actions>
+                <v-chip-group active-class="blue white--text">
+                  <v-chip class="ma-2">30 мин</v-chip>
+                  <v-chip class="ma-2">От 1 ₽</v-chip>
+                </v-chip-group>
+              </v-card-actions>
+            </v-card>
+          </div>
+      </v-container>
+      
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    Header
   },
 
   data: () => ({
@@ -55,3 +206,11 @@ export default Vue.extend({
   })
 });
 </script>
+
+<style lang="scss" scoped>
+.grid-container {
+  display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, 344px);
+}
+</style>
---
+++ arzeda.front/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app-bar app color="primary" dark>
+    <v-row
+      align="center"
+      no-gutters
+    >
+    <v-col lg="1" class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title>Arz.Еда</v-toolbar-title>
+    </v-col>
+    <v-col>
+      <v-text-field
+            label="Адрес доставки"
+            prepend-inner-icon="mdi-map-marker"
+            hide-details="auto"
+          ></v-text-field>
+    </v-col>
+    <v-col lg="1">
+      <v-btn
+        text
+        color="success"
+        class="mr-2"
+      >
+        <v-icon left>
+          mdi-cart-outline
+        </v-icon>
+      </v-btn>
+
+      <v-avatar
+        color="orange"
+        size="50"
+      >
+        <span class="white--text headline">JP</span>
+      </v-avatar>
+    </v-col>
+    </v-row>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {}
+</script>
