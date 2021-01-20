3fade679696b188d99436c94673c9fa9602ee48a
Aleksandra Grahova <grahova_sasha@mail.ru>
2021-01-20 22:22:25 +0300

    Добавила ссылку на главную страницу в шапке

--- arzeda.front/src/components/Header.vue
+++ arzeda.front/src/components/Header.vue
@@ -4,7 +4,7 @@
       align="center"
       no-gutters
     >
-    <v-col lg="1" class="d-flex align-center">
+    <v-col lg="1" class="d-flex align-center"><router-link to="/">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -12,7 +12,7 @@
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
+        /></router-link>
 
         <v-toolbar-title>Arz.Еда</v-toolbar-title>
     </v-col>
